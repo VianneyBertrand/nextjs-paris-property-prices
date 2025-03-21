67668cab637b25b6ba10b335d07b16d5d2ae84b0
VianneyBertrand <132300971+VianneyBertrand@users.noreply.github.com>
2025-03-21 01:06:00 +0100

    Add app layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Prix de l'Immobilier à Paris - Évolution Mensuelle",
+  description:
+    "Visualisation des prix de l'immobilier à Paris mois par mois depuis janvier 2020",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
